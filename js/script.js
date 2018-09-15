8dcc8b855c3555a6d3f3db7fc7b1907f85475283
Dinh <seredinh@34363bce4f2a.ant.amazon.com>
2018-09-15 13:21:39 -0700

    added if notes

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// if statement 
+if (1) {
+	console.log("good day!")
+}
+// if else statement 
+if (false) {
+	console.log("this won't work")
+}else {
+	console.log("this will work then")
+}
+
+if ("today is Saturday") {
+	console.log ("weekend!")
+} else if ("Sunday") {
+	console.log ("still weekend")
+} else {
+	console.log ("go to work!")
+}
