4984daa2d0885298e3f968f9f1bc1bd689875d89
Dinh <seredinh@34363bce4f2a.ant.amazon.com>
2018-09-15 14:38:07 -0700

    for loop notes and array

--- js/script.js
+++ js/script.js
@@ -6,30 +6,34 @@ if (1) {
 	console.log("good day!")
 }
 // if else statement 
-if (false) {
-	console.log("this won't work")
-}else {
-	console.log("this will work then")
-}
+// if (false) {
+// 	console.log("this won't work")
+// }else {
+// 	console.log("this will work then")
+// }
 
-if ("today is Saturday") {
-	console.log ("weekend!")
-} else if ("Sunday") {
-	console.log ("still weekend")
-} else {
-	console.log ("go to work!")
+// if ("today is Saturday") {
+// 	console.log ("weekend!")
+// } else if ("Sunday") {
+// 	console.log ("still weekend")
+// } else {
+// 	console.log ("go to work!")
+// }
+// //for loop
+// for (var i = 1; i <= 100; i = i+1) {
+// 	console.log (i)
+// }
+// //1-100: print even numbers
+// for (var i = 0; i <= 100; i = i+1) {
+// 	if (i % 2 === 0){
+// 		console.log(i+" is an even number")
+// 	}
+// 	else if (i % 2 !==0){
+// 		console.log(i+ " is an odd number")
+// 	}
+// }
+
+var friends = ["serena", "nick", "sarah", "jiaren", "max", "moomoo"]
+for (let i = 0; i < friends.length; i++) {
+	console.log(friends[i])
 }
-//for loop
-for (var i = 1; i <= 100; i = i+1) {
-	console.log (i)
-}
-//1-100: print even numbers
-for (var i = 0; i <= 100; i = i+1) {
-	if (i % 2 === 0){
-		console.log(i+" is an even number")
-	}
-	else if (i % 2 !==0){
-		console.log(i+ " is an odd number")
-	}
-}
-var name = ["serena", "nick", "sarah", "jiaren", "max", "moomoo"]
